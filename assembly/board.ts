ca9aef9049e16997b4062cdbac00f7881fb45a2f
mhonert <48153876+mhonert@users.noreply.github.com>
2020-03-01 17:51:56 +0100

    Adjust position score table to encourage pawns to advance

--- assembly/board.ts
+++ assembly/board.ts
@@ -834,8 +834,8 @@ function calculateEnPassantBitMask(bit: i32, index: i32, array: Array<i32>): i32
 export const PAWN_POSITION_SCORES: Int32Array = toInt32Array([
   0,  0,  0,  0,  0,  0,  0,  0,
   10, 10, 10, 10, 10, 10, 10, 10,
-  2,  2,  4,  6,  6,  4,  2,  2,
-  1,  1,  2,  5,  5,  2,  1,  1,
+  6,  6,  7,  8,  8,  7,  6,  6,
+  2,  2,  3,  5,  5,  3,  2,  2,
   0,  0,  0,  4,  4,  0,  0,  0,
   1, -1, -2,  0,  0, -2, -1,  1,
   1,  2,  2, -4, -4,  2,  2,  1,
